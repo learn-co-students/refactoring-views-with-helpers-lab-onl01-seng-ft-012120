3f521b46ed4a1d0591f8a82d606aeba022d9c814
reddevilcero <reddevil_cero@hotmail.com>
2020-03-20 17:01:07 +0000

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def display_artist(song)
+    # binding.pry
+    if song.artist_name.present?
+      link_to(song.artist_name, artist_path(song.artist))
+    else
+      link_to('Add Artist', edit_song_path(song))
+    end
+  end
 end
